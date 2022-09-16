ec7c51ac9c9cd17cb6e15c2ab80f5cd42cac76ea
Niclas Hedam <nhed@itu.dk>
2022-09-16 17:46:16 +0200

    Fixing paths and fixing bad comparison in map.h

--- files/include/functions/map.h
+++ files/include/functions/map.h
@@ -12,17 +12,18 @@ struct delilah_map_t
 };
 
 static struct delilah_map_t files[6] = {
-  { "1kb", "/media/nvme/1kb.tmp" },     { "10kb", "/media/nvme/10kb.tmp" },
-  { "100kb", "/media/nvme/100kb.tmp" }, { "1mb", "/media/nvme/1mb.tmp" },
-  { "10mb", "/media/nvme/10mb.tmp" },   { "100mb", "/media/nvme/100mb.tmp" },
+  { "1kb", "/media/nvme/100.txt" },     { "10kb", "/media/nvme/1000.txt" },
+  { "100kb", "/media/nvme/10000.txt" }, { "1mb", "/media/nvme/100000.txt" },
+  { "10mb", "/media/nvme/1000000.txt" },   { "100mb", "/media/nvme/10000000.txt" },
 };
 
 static char*
 delilah_functions_map(char* in)
 {
   for (int i = 0; i < 6; i++) {
-    if (strcmp(in, files[i].in))
+    if (strcmp(in, files[i].in) == 0){
       return files[i].out;
+    }
   }
 
   return NULL;
