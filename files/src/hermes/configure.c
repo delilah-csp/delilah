d669b4afdb4935cf62bb0f193721c817afe0b09b
Niclas Hedam <niclas@hed.am>
2023-03-08 17:18:25 +0100

    Add comments to src/hermes/configure.c

--- files/src/hermes/configure.c
+++ files/src/hermes/configure.c
@@ -14,18 +14,18 @@ delilah_hermes_configure(struct delilah_t* delilah)
 {
   struct hermes_bar0* bar0 = delilah->bar0;
 
-  bar0->ehver = 0;
-  bar0->eheng = HERMES_UBPF_ENGINES;
-  bar0->ehpslot = HERMES_PROG_SLOT_COUNT;
-  bar0->ehdslot = HERMES_DATA_SLOT_COUNT;
-  bar0->ehpsoff = HERMES_DATA_LOC;
-  bar0->ehpssze = HERMES_PROG_SLOT_SIZE;
-  bar0->ehdsoff =
-    HERMES_DATA_LOC + (HERMES_PROG_SLOT_COUNT * HERMES_PROG_SLOT_SIZE);
-  bar0->ehdssze = HERMES_DATA_SLOT_SIZE;
+  bar0->ehver = 0;                        // Protocol version
+  bar0->eheng = HERMES_UBPF_ENGINES;      // Number of engines
+  bar0->ehpslot = HERMES_PROG_SLOT_COUNT; // Number of program slots
+  bar0->ehdslot = HERMES_DATA_SLOT_COUNT; // Number of data slots
+  bar0->ehpsoff = HERMES_DATA_LOC;        // Program slot offset
+  bar0->ehpssze = HERMES_PROG_SLOT_SIZE;  // Program slot size
+  bar0->ehdsoff = HERMES_DATA_LOC + (HERMES_PROG_SLOT_COUNT *
+                                     HERMES_PROG_SLOT_SIZE); // Data slot offset
+  bar0->ehdssze = HERMES_DATA_SLOT_SIZE;                     // Data slot size
 
   snprintf(bar0->ehbld, 48, "Delilah v%i.%i.%i", DELILAH_VERSION_MAJOR,
-           DELILAH_VERSION_MINOR, DELILAH_VERSION_BUILD);
+           DELILAH_VERSION_MINOR, DELILAH_VERSION_BUILD); // Delilah version
 
   return 0x0;
 }
