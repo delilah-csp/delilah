8f4334e7457632100e43230adf0773e2991e44aa
Niclas Hedam <niclas@hed.am>
2023-03-13 11:02:39 +0100

    Add FS module to enable programs to mount and unmount drives

--- files/include/functions/index.h
+++ files/include/functions/index.h
@@ -2,8 +2,7 @@
 #define DELILAH_FUNCTIONS_INDEX
 
 #include "functions/file.h"
-#include "functions/map.h"
-#include "functions/sort.h"
+#include "functions/fs.h"
 #include "functions/version.h"
 
 struct ext_func
@@ -15,24 +14,22 @@ struct ext_func
 
 static struct ext_func registered_functions[] = {
   /* Version Module */
-  {.idx = 0,
-   .name = "delilah_version_major",
-   &delilah_functions_version_major },
-  {.idx = 1,
-   .name = "delilah_version_minor",
-   &delilah_functions_version_minor },
-  {.idx = 2,
-   .name = "delilah_version_build",
-   &delilah_functions_version_build },
+  { .idx = 0,
+    .name = "delilah_version_major",
+    &delilah_functions_version_major },
+  { .idx = 1,
+    .name = "delilah_version_minor",
+    &delilah_functions_version_minor },
+  { .idx = 2,
+    .name = "delilah_version_build",
+    &delilah_functions_version_build },
 
   /* File Module */
-  {.idx = 10, .name = "delilah_file_read", &delilah_functions_file_read },
+  { .idx = 10, .name = "delilah_file_read", &delilah_functions_file_read },
 
-  /* Sort Module */
-  {.idx = 20, .name = "delilah_sort_string", &delilah_functions_sort_string },
-
-  /* Map Module */
-  {.idx = 30, .name = "delilah_map", &delilah_functions_map }
+  /* FS Module */
+  { .idx = 20, .name = "delilah_fs_mount", &delilah_functions_fs_mount },
+  { .idx = 21, .name = "delilah_fs_unmount", &delilah_functions_fs_unmount }
 };
 
 #endif
