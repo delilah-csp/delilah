2ecc89fd3da70635ba20ff1abe959f6d902a2825
Niclas Hedam <niclas@hed.am>
2023-05-03 12:25:15 +0200

    Implement offset-based reads and CPU prefetching registered functions

--- files/include/functions/file.h
+++ files/include/functions/file.h
@@ -25,4 +25,37 @@ delilah_functions_file_read(void* buffer, size_t size, char* filename)
   return (fsize < size - 1) ? fsize : size;
 }
 
+static size_t
+delilah_functions_file_read_offset(void* buffer, size_t size, size_t offset,
+                                   char* filename)
+{
+  size_t i = 0;
+  size_t fsize;
+  char* buf = (char*)buffer;
+  FILE* f = fopen(filename, "rb");
+
+  // Set the file position to the given offset
+  if (fseek(f, offset, SEEK_SET) != 0) {
+    fclose(f);
+    return 0;
+  }
+
+  fseek(f, 0, SEEK_END);
+  fsize = ftell(f);
+
+  // Ensure the offset is within the file size
+  if (offset > fsize) {
+    fclose(f);
+    return 0;
+  }
+
+  fseek(f, offset, SEEK_SET);
+  size_t read_size = (fsize - offset < size - 1) ? fsize - offset : size - 1;
+  size_t result = fread(buf, 1, read_size, f);
+  fclose(f);
+
+  buf[result] = 0;
+  return result;
+}
+
 #endif
--- files/include/functions/index.h
+++ files/include/functions/index.h
@@ -1,6 +1,7 @@
 #ifndef DELILAH_FUNCTIONS_INDEX
 #define DELILAH_FUNCTIONS_INDEX
 
+#include "functions/cpu.h"
 #include "functions/file.h"
 #include "functions/fs.h"
 #include "functions/version.h"
@@ -26,10 +27,19 @@ static struct ext_func registered_functions[] = {
 
   /* File Module */
   { .idx = 10, .name = "delilah_file_read", &delilah_functions_file_read },
+  { .idx = 11,
+    .name = "delilah_file_read_offset",
+    &delilah_functions_file_read_offset },
 
   /* FS Module */
   { .idx = 20, .name = "delilah_fs_mount", &delilah_functions_fs_mount },
-  { .idx = 21, .name = "delilah_fs_unmount", &delilah_functions_fs_unmount }
+  { .idx = 21, .name = "delilah_fs_unmount", &delilah_functions_fs_unmount },
+
+  /* CPU Module */
+  { .idx = 30, .name = "delilah_cpu_prefetch_rl1", &delilah_functions_cpu_prefetch_rl1 },
+  { .idx = 31, .name = "delilah_cpu_prefetch_rl2", &delilah_functions_cpu_prefetch_rl2 },
+  { .idx = 32, .name = "delilah_cpu_prefetch_wl1", &delilah_functions_cpu_prefetch_wl1 },
+  { .idx = 33, .name = "delilah_cpu_prefetch_wl2", &delilah_functions_cpu_prefetch_wl2 }
 };
 
 #endif
