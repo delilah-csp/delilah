7ea5ee86f16b4db2f511a962425fe1faf5f3f1d3
Niclas Hedam <niclas@hed.am>
2023-07-31 13:47:42 +0200

    Implement clear cache command

---
+++ files/src/commands/clear_cache.c
@@ -0,0 +1,39 @@
+#include "commands/commands.h"
+#include "hermes/config.h"
+#include "hermes/status.h"
+#include "mem/mem.h"
+#include "util/log.h"
+#include "util/time.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
+#include <string.h>
+#include <sys/time.h>
+
+uint64_t
+delilah_command_clear_cache(struct delilah_thread_t* thread,
+                        struct hermes_cmd_req* req, struct hermes_cmd_res* res,
+                        struct delilah_t* delilah)
+{
+    int fd = open("/proc/sys/vm/drop_caches", O_WRONLY);
+    if (fd == -1) {
+        log_error("Error opening /proc/sys/vm/drop_caches");
+        return -1;
+    }
+
+    // Writing "3" to drop_caches to clear both page cache and inode cache
+    const char *data = "3";
+    if (write(fd, data, sizeof(char)) == -1) {
+        log_error("Error writing to /proc/sys/vm/drop_caches");
+        close(fd);
+        return -1;
+    }
+
+    close(fd);
+
+    res->status = HERMES_STATUS_SUCCESS;
+
+    return 0;
+}
