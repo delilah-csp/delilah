c58957890cee8eddf2c1ce322e0bbe80441062f2
Niclas Hedam <nhed@itu.dk>
2023-09-12 11:58:16 +0200

    Implement meta get/setters

--- files/include/loader/loader.h
+++ files/include/loader/loader.h
@@ -4,6 +4,7 @@
 #include "delilah.h"
 #include <pthread.h>
 #include <sched.h>
+#include <stdint.h>
 
 struct delilah_thread_t
 {
@@ -14,6 +15,8 @@ struct delilah_thread_t
   struct hermes_cmd* cmd;
   struct delilah_cmd_ctrl* ctrl;
 };
+
+static __thread uint32_t thread_meta;
 
 return_t delilah_loader_configure(struct delilah_t* delilah);
 return_t delilah_loader_unload(struct delilah_t* delilah);
